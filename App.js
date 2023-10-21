374d996d2b906872f6dd2700fbcf28a04df9e33a
Tak <takeshiseodev@gmail.com>
2023-10-21 19:14:09 -0400

    general refactor

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Layout from "./components/Layout"
+
+function App() {
+    return (
+        <Layout />
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
